8965d06d722ed917a7177bed9ffc173ed7de762d
corleroux <corleroux@users.noreply.github.com>
2022-03-18 15:56:07 +0000

    deploy: 65b2f0d6f8aab1ddc015c909d5689741d96d742e

--- span-api-proxy/index.test.js
+++
@@ -1,8 +0,0 @@
-import { handler } from "./index";
-
-describe("Lambda function", () => {
-  it("should return with programming cliché", async () => {
-    const result = await handler();
-    expect(result).toBe("Hello world");
-  });
-});
